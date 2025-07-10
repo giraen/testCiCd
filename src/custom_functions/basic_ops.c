2615dce510bb76490c9affb573f83f1745161c0d
giraen <122036189+giraen@users.noreply.github.com>
2025-07-10 20:22:21 +0800

    basic unit testing without framework

---
+++ src/custom_functions/basic_ops.c
@@ -0,0 +1,25 @@
+#include "basic_ops.h"
+
+double add(double x, double y)
+{
+    return x + y;
+}
+
+double subtract(double x, double y)
+{
+    return x - y;
+}
+
+double multiply(double x, double y)
+{
+    return x * y;
+}
+
+double divide(double x, double y)
+{
+    if (x == 0 || y == 0)
+    {
+        return 0;
+    }
+    return x / y;
+}
---
+++ src/custom_functions/basic_ops.h
@@ -0,0 +1,9 @@
+#ifndef BASIC_OPS_H
+#define BASIC_OPS_H
+
+double add(double x, double y);
+double subtract(double x, double y);
+double multiply(double x, double y);
+double divide(double x, double y);
+
+#endif
