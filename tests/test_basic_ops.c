60f2543c0129b36d0a5455c8aa61a447c9e1678d
giraen <122036189+giraen@users.noreply.github.com>
2025-07-10 20:41:27 +0800

    this shouldn't work

--- tests/test_basic_ops.c
+++ tests/test_basic_ops.c
@@ -3,10 +3,17 @@
 
 int main() {
     
+    // Should work
     assert(add(2, 3) == 5);
-    assert(add(0, 0) == 0);
-    assert(add(-1, 0) == -1);
-    assert(add(-1, 1) == 0);
+    assert(subtract(100, 12) == 88);
+    assert(multiply(-1, 0) == 0);
+    assert(divide(-1, 1) == -1);
+
+    // Shouldn't work
+    assert(add(2, 3, 3) == 8);
+    assert(subtract(100, "12") == 88);
+    assert(multiply(-1) == 0);
+    
 
     return 0;
 }
